91740299d2db06988a422fcf1f9ea25711c0e3d7
tigrannmkrtchyan97-eng <tigrannmkrtchyan97@gmail.com>
2025-10-04 20:18:17 +0400

    solution to problems

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-import dbconnect from 'mongodb-ci'
 
-
-export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://dulanjalisenarathna93:E2JUb0zfaT2FVp8D@cluster0.exkxkun.mongodb.net/reactjs-food-delivery-app').then(()=>{
-        console.log('DB connected') ;
-        dbconnect();
-    })
-}
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION_STRING);
+        console.log('DB connected');
+    } catch (error) {
+        console.error('DB connection failed:', error);
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(cors())
 
 //db connection
-connectDB();
+await connectDB();
 // api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
